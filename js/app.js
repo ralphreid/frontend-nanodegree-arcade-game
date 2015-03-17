3f8a2307ed2281d8de001b8a2c167edff69402a4
Ralph <beresfordjunior@me.com>
2015-03-17 11:39:00 +0200

    start working on enemy object

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
